84c517df3578f2b61d29f6996ef74298eb11320e
deshydf <155445472+yash-deshpande24@users.noreply.github.com>
2025-09-11 16:48:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
